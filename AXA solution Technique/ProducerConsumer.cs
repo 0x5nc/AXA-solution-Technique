2d7293430c9d82bb3543ee4b7a204f0884a0b490
Bulls3y3 <slim.essency@gmail.com>
2021-01-28 17:21:20 +0100

    Create the Managing Class ProducerConsumer

---
+++ AXA solution Technique/ProducerConsumer.cs
@@ -0,0 +1,23 @@
+﻿using AXA_solution_Technique.Interfaces;
+using System.Threading.Tasks;
+
+namespace AXA_solution_Technique
+{
+    internal class ProducerConsumer : IProducerConsumer
+    {
+        public void ReadFile()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task PrintFile()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task EditFile()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- AXA solution Technique/Program.cs
+++ AXA solution Technique/Program.cs
@@ -1,12 +1,20 @@
-﻿using System;
-
-namespace AXA_solution_Technique
+﻿namespace AXA_solution_Technique
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Not implementing Dependency Injection for simplicity purposes
+            ProducerConsumer producerConsumer = new ProducerConsumer();
+
+            //Reading the simulated large txt file input
+            producerConsumer.ReadFile();
+
+            //Printing File then Editing for the implemented lock 
+            producerConsumer.PrintFile();
+            producerConsumer.EditFile();
+            producerConsumer.PrintFile();
+            producerConsumer.EditFile();
         }
     }
 }
