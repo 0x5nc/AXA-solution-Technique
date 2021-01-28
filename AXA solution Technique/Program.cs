1674b2d6010e419e8454c7b7105a3c4d9f1a88d6
Bulls3y3 <slim.essency@gmail.com>
2021-01-28 17:16:08 +0100

    Add project files.

---
+++ AXA solution Technique/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AXA_solution_Technique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
