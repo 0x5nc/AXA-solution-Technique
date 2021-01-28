6d0bf700dc317698896e7e7d9a0032d40bad448c
Bulls3y3 <slim.essency@gmail.com>
2021-01-28 17:31:55 +0100

    Fixing file reading issue.

--- AXA solution Technique/ProducerConsumer.cs
+++ AXA solution Technique/ProducerConsumer.cs
@@ -8,26 +8,34 @@ namespace AXA_solution_Technique
     internal class ProducerConsumer : IProducerConsumer
     {
         private string _path;
-        private StreamReader _streamReader;
         private object fileLock = new object();
+        private StreamReader _streamReader;
+
+        //Creating a StreamReader for the bufferedStream 
         public void ReadFile()
         {
             lock (fileLock)
             {
                 Console.WriteLine("Enter the file path : ");
                 _path = Console.ReadLine();
-                FileStream fs = File.Open(_path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
-                BufferedStream bs = new BufferedStream(fs);
-                _streamReader = new StreamReader(bs);
+                OpenFile();
             }
         }
 
+        private void OpenFile()
+        {
+            FileStream fs = File.Open(_path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            BufferedStream bs = new BufferedStream(fs);
+            _streamReader = new StreamReader(bs);
+        }
+
+        //Printing the content
         public async Task PrintFile()
         {
+            OpenFile();
+            string line;
             lock (fileLock)
             {
-                string line;
-
                 Console.WriteLine("Printing the locked file");
 
                 while ((line = _streamReader.ReadLine()) != null)
@@ -36,6 +44,7 @@ namespace AXA_solution_Technique
                 }
             }
             await Task.Delay(5000);
+
         }
 
         public async Task EditFile()
