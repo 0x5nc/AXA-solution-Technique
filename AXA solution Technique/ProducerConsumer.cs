2989a80c830abd3a60b237d455e485cf19791a70
Bulls3y3 <slim.essency@gmail.com>
2021-01-28 17:23:35 +0100

    Implementing the BufferedStream file reading Method

--- AXA solution Technique/ProducerConsumer.cs
+++ AXA solution Technique/ProducerConsumer.cs
@@ -1,13 +1,23 @@
 ﻿using AXA_solution_Technique.Interfaces;
+using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace AXA_solution_Technique
 {
     internal class ProducerConsumer : IProducerConsumer
     {
+        private string _path;
+        private StreamReader _streamReader;
         public void ReadFile()
         {
-            throw new System.NotImplementedException();
+            
+            Console.WriteLine("Enter the file path : ");
+            _path = Console.ReadLine();
+            FileStream fs = File.Open(_path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            BufferedStream bs = new BufferedStream(fs);
+            _streamReader = new StreamReader(bs);
+
         }
 
         public Task PrintFile()
